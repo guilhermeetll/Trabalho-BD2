720211ff571f3b80a03a06d33cdeb4553d638511
Matheus Silva <scruz.math@gmail.com>
2026-06-29 16:51:10 -0300

    crianção da estrutura da loja

---
+++ loja-virtual/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div style={{ padding: '2rem' }}>
+            <h1>Painel do Vendedor</h1>
+            <p>Se você está vendo essa página, o projeto novo está rodando certinho.</p>
+        </div>
+    );
+}
+
+export default App;
